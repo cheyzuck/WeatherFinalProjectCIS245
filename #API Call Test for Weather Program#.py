eaddfb140208b9a2189e8ce7b80874bb3d590048
Cheyenne <105511806+cheyzuck@users.noreply.github.com>
2022-05-20 16:41:32 -0400

    Update #API Call Test for Weather Program#.py

--- #API Call Test for Weather Program#.py
+++ #API Call Test for Weather Program#.py
@@ -6,10 +6,15 @@ from pip._vendor import requests
 api_token = '81ab2c5b7610ea0509218bc424ec2ec5'
 api_url_base = 'https://api.openweathermap.org/'
 
-url = 'https://api.openweathermap.org/data/2.5/weather?zip=17551,&appid=81ab2c5b7610ea0509218bc424ec2ec5'
+zip_code = input("What is your zip code?"
+
+url = 'https://api.openweathermap.org/data/2.5/weather?zip='+zip_code+',&appid=81ab2c5b7610ea0509218bc424ec2ec5'
 headers = {'Accept' : '81ab2c5b7610ea0509218bc424ec2ec5'}
 r = requests.get(url, headers=headers)
 print(f"Status code: {r.status_code}")
 response_dict = r.json()
 print(response_dict.keys())
 print(f"Current Weather: {response_dict['weather']}")
+                 
+coord = {response_dict['coord']}
+print(coord)
